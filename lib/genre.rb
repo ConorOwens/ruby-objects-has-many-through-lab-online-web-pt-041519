2e27862a7fda026f16ee266f2755fd1cb902198f
Conor Owens <conorj.owens@gmail.com>
2019-05-25 18:37:23 +0000

    Automatically backed up by Learn

---
+++ lib/genre.rb
@@ -0,0 +1,20 @@
+class Genre 
+  
+  @@all = []
+  
+  attr_accessor :name, :songs
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def artists
+    
+  
+end
